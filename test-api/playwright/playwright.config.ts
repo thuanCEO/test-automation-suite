523ce9ddf762037ebacb4a1141f1d40ee290d404
thuan.do <thuan.do@agest.vn>
2025-08-06 16:00:35 +0700

    add api playwright

---
+++ test-api/playwright/playwright.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from '@playwright/test';
+import { Reporter } from '../playwright/src/constants/Reporter';
+import { DriverConfig } from '../playwright/src/config/DriverConfig';
+
+export default defineConfig({
+  testDir: DriverConfig.testDir,
+  timeout: TimeOut.XLONG,
+  retries: DriverConfig.retries,
+  workers: DriverConfig.workers,
+  expect: {
+    timeout: TimeOut.DEFAULT,
+  },
+  reporter: Reporter.reporter,
+  name: DriverConfig.name,
+  use: {
+    browserName: DriverConfig.browserName,
+    channel: DriverConfig.channel,
+    headless: DriverConfig.headless,
+    ignoreHTTPSErrors: DriverConfig.ignoreHTTPSErrors,
+    screenshot: DriverConfig.screenshot,
+    video: DriverConfig.video,
+    trace: DriverConfig.trace,
+  }
+});
---
+++ test-api/playwright/src/services/AuthService.ts
@@ -0,0 +1,14 @@
+import { ApiClient } from '../core/ApiClient';
+import { setToken } from '../core/TokenManager';
+
+const api = new ApiClient();
+
+export async function login(email: string, password: string) {
+    const res = await api.post('/auth/login', { email, password });
+
+    if (res.status === 200 && res.body.token) {
+        setToken(res.body.token);
+    }
+
+    return res;
+}
