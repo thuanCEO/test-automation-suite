fe5692a1388853a9761ae4a8f14554b632417b85
thuan.do <thuan.do@agest.vn>
2025-08-07 11:24:09 +0700

    Add karate framework

--- test-api/playwright/src/config/ApiConfig.ts
+++
@@ -1,8 +0,0 @@
-// src/config/apiConfig.ts
-export const ApiConfig = {
-    baseURL: 'https://example.com/api',
-    defaultHeaders: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
-};
